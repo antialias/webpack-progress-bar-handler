ba3e2f543fa0eef89fb9a3c8d033d5d97d138baa
Thomas Hallock <thomas@1stdibs.com>
2015-09-29 17:53:57 -0400

    configuration options

--- webpack-progress-bar-handler.js
+++ webpack-progress-bar-handler.js
@@ -1,14 +1,26 @@
 var readline = require('readline');
-module.exports = function (percentage, message) {
-    readline.clearLine(process.stdout, 0); // clear current text
-    readline.cursorTo(process.stdout, 0); // move cursor to beginning of line
-    var progressLength = 50;
-    var arrowLength = Math.floor(percentage * progressLength);
-    var arrowHead = '>';
-    var arrowBody = '=';
-    if (percentage === 1) {
-        arrowHead = arrowBody;
-        message += "\n";
+var assign = require('lodash.assign');
+var throttle = require('lodash.throttle');
+module.exports = function (options) {
+    options = extend({
+        progressLength: 50,
+        arrowHead: '>',
+        arrowBody: '=',
+        unfinished: '-',
+        throttle: 0;
+    }, options);
+    var handler = function (percentage, message) {
+        var arrowLength = Math.floor(percentage * options.progressLength);
+        readline.clearLine(process.stdout, 0); // clear current text
+        readline.cursorTo(process.stdout, 0); // move cursor to beginning of line
+        if (percentage === 1) {
+            options.arrowHead = options.arrowBody;
+            message += "\n";
+        }
+        process.stdout.write('[webpack] : [' + (new Array(arrowLength).join(options.arrowBody)) + options.arrowHead + (new Array(options.progressLength - arrowLength).join('-')) + '] ' + (percentage * 100).toFixed(2) + '% : ' + message);
+    };
+    if (options.throttle > 0) {
+        handler = throttle(handler, options.throttle);
     }
-    process.stdout.write('[webpack] : [' + (new Array(arrowLength).join(arrowBody)) + arrowHead + (new Array(progressLength - arrowLength).join('-')) + '] ' + (percentage * 100).toFixed(2) + '% : ' + message);
-}
+    return handler;
+};
